8426233ba75f83a773ee9eed7faead4606e7b2b5
00007518 <00007518@uca.edu.sv>
2024-07-23 18:22:00 -0600

    feat: add the methods and the file to make the filter of the requests :sparkles:

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,130 +1,116 @@
 //importe de las funciones utilizadas para administrar la db
 const db = require('../db/db_connection');
-
+const filter = require('../utils/filterOptions')
 //función para obtener todas las solicitudes hechas en este nuevo ciclo
 const getRequests = async (req, res) => {
-    //obteniendo información principal
-    const requests = await db.select('*').table('rcrt_all_elements')
-        .limit(10).orderBy('id', 'asc')
 
-    //obteniendo identificadores de las solicitudes
-    const elementsData = requests.map((object) => {
-        return object.id
-    })
-    //obteniendo la data complementario de cada solicitud
-    const complementaryData = await Promise.all(elementsData.map(async (id) => {
-        return data = await db('rcrt_elements_data').where('element_id', id)
-    }))
-    //variable para sacar los items de la data complementaria
-    //esto se hace porque la data complementaria trae este formato:
-    // [[{},{},{}],[{},{},{}]]
-    const items = []
+    try {
+        let requests = []
 
-    complementaryData.forEach(data => {
-        data.forEach(item => items.push(item))
-    })
+        const { bookingOption, cycle,
+            signature, firstOption, secondOption } = req.query
 
-    //anidando toda la data que se necesita para luego enviarla
-    const sendData = requests.map(request => {
-        const data = items.filter((item) => item.element_id === request.id)
+        //validación para controlar que se quiere filtrar por tipo de 
+        //contratación
+        if (bookingOption) {
 
-        if (data) {
-            return { ...request, data }
-        } else {
-            return request
-        }
-    })
-
-    if (sendData) {
-        return res.status(200).json({ "data": sendData })
-    } else {
-        return res.status(404).json({ message: 'solicitudes no encontradas' })
-    }
-
-}
-
-const getRequestsByBooking = async (req, res) => {
-    const bookingOption = req.query.option
-
-    if (bookingOption == 'Remunerado') {
-        const requests = await db().select('*').table('rcrt_all_elements')
-            .where('es_remunerado', 1).limit(10).orderBy('id', 'asc')
-
-        //obteniendo identificadores de las solicitudes
-        const elementsData = requests.map((object) => {
-            return object.id
-        })
-        //obteniendo la data complementario de cada solicitud
-        const complementaryData = await Promise.all(elementsData.map(async (id) => {
-            return data = await db('rcrt_elements_data').where('element_id', id)
-        }))
-        //variable para sacar los items de la data complementaria
-        //esto se hace porque la data complementaria trae este formato:
-        // [[{},{},{}],[{},{},{}]]
-        const items = []
-
-        complementaryData.forEach(data => {
-            data.forEach(item => items.push(item))
-        })
-
-        //anidando toda la data que se necesita para luego enviarla
-        const sendData = requests.map(request => {
-            const data = items.filter((item) => item.element_id === request.id)
-
-            if (data) {
-                return { ...request, data }
+            const sendData = await filter.filterByBooking(bookingOption)
+            if (sendData) {
+                return res.status(200).json({ "data": sendData })
             } else {
-                return request
+                return res.status(404).json({ message: 'solicitudes no encontradas' })
             }
-        })
 
-        if (sendData) {
-            return res.status(200).json({ "data": sendData })
-        } else {
-            return res.status(404).json({ message: 'solicitudes no encontradas' })
-        }
-    } else {
-        const requests = await db().select('*').table('rcrt_all_elements')
-            .where('es_remunerado', 0).limit(10).orderBy('id', 'asc')
+        } else if (cycle) {
 
-        //obteniendo identificadores de las solicitudes
-        const elementsData = requests.map((object) => {
-            return object.id
-        })
-        //obteniendo la data complementario de cada solicitud
-        const complementaryData = await Promise.all(elementsData.map(async (id) => {
-            return data = await db('rcrt_elements_data').where('element_id', id)
-        }))
-        //variable para sacar los items de la data complementaria
-        //esto se hace porque la data complementaria trae este formato:
-        // [[{},{},{}],[{},{},{}]]
-        const items = []
-
-        complementaryData.forEach(data => {
-            data.forEach(item => items.push(item))
-        })
-
-        //anidando toda la data que se necesita para luego enviarla
-        const sendData = requests.map(request => {
-            const data = items.filter((item) => item.element_id === request.id)
-
-            if (data) {
-                return { ...request, data }
+            const sendData = await filter.filterByCycle(cycle)
+            if (sendData) {
+                return res.status(200).json({ "data": sendData })
             } else {
-                return request
+                return res.status(404).json({ message: 'solicitudes no encontradas' })
             }
-        })
 
-        if (sendData) {
-            return res.status(200).json({ "data": sendData })
+        } else if (signature) {
+
+            if (firstOption && secondOption) {
+                console.log("entra en primera opción y segunda opción")
+                const sendData = await filter.filterBySignature(signature, [firstOption, secondOption])
+                if (sendData) {
+                    return res.status(200).json({ "data": sendData })
+                } else {
+                    return res.status(404).json({ message: 'solicitudes no encontradas' })
+                }
+
+
+            } else if (firstOption) {
+
+                const sendData = await filter.filterBySignature(signature, [firstOption])
+                if (sendData) {
+                    return res.status(200).json({ "data": sendData })
+                } else {
+                    return res.status(404).json({ message: 'solicitudes no encontradas' })
+                }
+
+            } else if (secondOption) {   
+
+                const sendData = await filter.filterBySignature(signature, [secondOption])
+                if (sendData) {
+                    return res.status(200).json({ "data": sendData })
+                } else {
+                    return res.status(404).json({ message: 'solicitudes no encontradas' })
+                }
+            }
+
         } else {
-            return res.status(404).json({ message: 'solicitudes no encontradas' })
-        }
-    }
 
+            //obteniendo información principal
+            requests = await db('rcrt_all_elements')
+                .select('*')
+                .limit(10)
+                .orderBy('id', 'asc');
+
+            //obteniendo identificadores de las solicitudes
+            const elementsData = requests.map((object) => {
+                return object.id
+            })
+            //obteniendo la data complementario de cada solicitud
+            const complementaryData = await Promise.all(elementsData.map(async (id) => {
+                return data = await db('rcrt_elements_data').where('element_id', id)
+            }))
+
+            //variable para sacar los items de la data complementaria
+            //esto se hace porque la data complementaria trae este formato:
+            // [[{},{},{}],[{},{},{}]]
+            const items = []
+
+            complementaryData.forEach(data => {
+                data.forEach(item => items.push(item))
+            })
+
+            //anidando toda la data que se necesita para luego enviarla
+            const sendData = requests.map(request => {
+                const data = items.filter((item) => item.element_id === request.id)
+
+                if (data) {
+                    return { ...request, data }
+                } else {
+                    return request
+                }
+            })
+
+            if (sendData) {
+                return res.status(200).json({ "data": sendData })
+            } else {
+                return res.status(404).json({ message: 'solicitudes no encontradas' })
+            }
+        }
+
+    } catch (error) {
+        console.error('Error al obtener las solicitudes: ', error)
+        return res.status(500).json({ message: 'Error al obtener las solicitudes' })
+    }
 }
 
 module.exports = {
     getRequests,
-    getRequestsByBooking
 }
